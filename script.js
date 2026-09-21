2ee39e08982d75c449886bd2aa5fd7232e73e843
Vasu-1001 <deva10012006@gmail.com>
2026-09-21 20:34:40 +0530

    Add HTML and JavaScript files

---
+++ script.js
@@ -0,0 +1,32 @@
+// /*  let variable = "vasudevan";
+//      vasu = "12345"
+
+//     abad = 23;
+//     console.log(typeof);
+// console.log(variable, "Hello, World!", vasu);
+//   */
+
+// let variable = 54;
+// {
+
+// if(variable > 80)
+// {
+//     console.log("A");
+// }
+
+// else if(variable > 60)
+// {
+//     console.log("B");
+// }
+// else if(variable > 50){
+//     console.log("C")
+// }
+// else{
+//     console.log("Fail");
+// }
+
+// }
+// console.log(typeof variable);
+let veges = ["carroet", "beetroot", {name : "goms"}, "cabbage", "tomato", "onion", "potato","5" , false];
+veges[4] = "tomato";
+console.log(veges[4]); 
